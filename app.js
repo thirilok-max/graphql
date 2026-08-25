5bde1146adb851f66d6234fdcc7cbdf2e823a3ca
thirilok-max <thirilok@webizze.com>
2026-08-25 13:54:54 +0530

    added

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-import express from "express"
-import express from "express-graphql"
+const express=require("express");
+const {graphqlHTTP}=require("express-graphql");
+const {buildSechma}=require("graphql");
+
+const User=[
+    {
+        id:"1",
+        name:"thirilok",
+        email:"thirilok@gmail.com",
+        password:"thirilok@123"
+    },
+    {
+        id:"2",
+        name:"tharun",
+        email:"tharun@gmail.com",
+        password:"tharun@123"
+    }
+]
